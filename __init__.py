0cf8cadae7f6663d5b7ca3583c0b604d84115a56
Axl11 <axlville5@gmail.com>
2024-06-19 15:48:50 -0600

    feat: se crea vista personalizada para manejar el error 404

--- __init__.py
+++ __init__.py
@@ -94,5 +94,10 @@ def create_app():
     @app.route('/')
     def index():
         return redirect(url_for('auth.login'))
+    
+    # Manejador de error 404 [Rutas no definidas]
+    @app.errorhandler(404)
+    def pagina_no_encontrada(error):
+        return render_template('404.html'), 404
 
     return app
