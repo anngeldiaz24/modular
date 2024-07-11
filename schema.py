4f9176142fffcc88a23d62a1cc27bd3ac0cb7019
Axl11 <axlville5@gmail.com>
2024-07-11 14:38:05 -0600

    fix: schema actualizado

--- schema.py
+++ schema.py
@@ -48,11 +48,13 @@ instructions = [
             FOREIGN KEY (codigo_acceso) REFERENCES codigos_acceso(id),
             FOREIGN KEY (hogar_id) REFERENCES hogares(id)
         );
-        
+    """,
+    """
         -- Creación de la tabla estados
         CREATE TABLE estados (
             id INT(10) AUTO_INCREMENT PRIMARY KEY,
-            nombre VARCHAR(255) NOT NULL      
+            nombre VARCHAR(255) NOT NULL
+        );      
     """,
     """
         -- Creación de la tabla eventos
