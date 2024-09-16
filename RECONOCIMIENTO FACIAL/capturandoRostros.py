0813700f30098c442bd727a7b9d6c3406aba1d31
Angel Diaz <angel.diaz5609@alumnos.udg.mx>
2024-09-15 18:43:21 -0600

    updated file

--- RECONOCIMIENTO FACIAL/capturandoRostros.py
+++ RECONOCIMIENTO FACIAL/capturandoRostros.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import imutils
 
-personName = 'lalo'
+personName = 'ana_garcia'
 dataPath = 'C:\\Users\\Angel Diaz\\Desktop\\Modular\\Data' #Cambia a la ruta donde hayas almacenado Data
 personPath = dataPath + '/' + personName
 
@@ -11,7 +11,7 @@ if not os.path.exists(personPath):
 	os.makedirs(personPath)
 
 #cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-cap = cv2.VideoCapture('lalo-prueba.mp4')
+cap = cv2.VideoCapture('static/videos/hogar_1/ana_garcia.mp4')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 count = 0
